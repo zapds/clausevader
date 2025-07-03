bd0614f688e84aad85734e62bcf28dc305d58eb7
zapds <71247896+zapds@users.noreply.github.com>
2025-07-04 03:50:36 +0530

    change domain

--- frontend/src/lib/server/oauth.js
+++ frontend/src/lib/server/oauth.js
@@ -3,5 +3,5 @@ import { Google } from "arctic";
 export const google = new Google(
 	process.env.GOOGLE_CLIENT_ID ?? "",
 	process.env.GOOGLE_CLIENT_SECRET ?? "",
-	"http://localhost:3000/login/google/callback"
+	"https://clausevader.vercel.app/login/google/callback"
 );
