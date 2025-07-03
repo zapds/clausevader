57ea20408ef2948b374422702d2cf15ffde64102
zapds <71247896+zapds@users.noreply.github.com>
2025-07-04 03:46:19 +0530

    fix bgu

--- frontend/src/app/logout/route.js
+++ frontend/src/app/logout/route.js
@@ -3,16 +3,12 @@
 import { getCurrentSession, invalidateSession, deleteSessionTokenCookie } from "@/lib/server/session";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import { useRouter } from "next/router";
 export async function GET() {
 	const { session } = await getCurrentSession();
 	if (session) {
 		const cookieStore = cookies();
 		await invalidateSession(session.id);
 		await deleteSessionTokenCookie(cookieStore);
-		const router = useRouter();
-		router.reload();
-
 	}
 	// Always redirect to login (even if not logged in)
 	return NextResponse.redirect(new URL("/", process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"));
